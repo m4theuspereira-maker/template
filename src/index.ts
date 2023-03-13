454f1834c8e3ee98a38af6d30e015d1820bbbcf1
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-03-13 14:07:02 -0300

    feat: add constants env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import { PORT } from "./config/environment-consts";
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-const server = app.listen('3000',()=>{})
+const server = app.listen(PORT, () => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  console.log(`listening on port ${PORT} 🚀`);
+});
 
-export { server}
+export { server };
