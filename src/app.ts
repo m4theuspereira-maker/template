a0708b42010bff7aad78198029fdc1647bcc0cbc
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-07-08 10:33:44 -0300

    refactor: add export after attribution

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express, { json, urlencoded } from "express";
 import cors from "cors";
 import { routes } from "./routes";
 
-export const app = express();
+const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cors());
 app.use(routes);
+
+export { app };
